66fea6b9fa80586f8b319ec48044b9991f5de544
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-07-14 17:03:16 +0600

    SumTillN

--- Main.java
+++ Main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
-        for(int i=10; i>1; i--) {
-            System.out.println("The value of i is: " +i);
+        Scanner scn = new Scanner(System.in);
+
+        // What to enter:
+        // Enter an integer n (example: 5)
+        // Program will calculate: 1 + 2 + 3 + ... + n
+        System.out.print("Enter a number (n): ");
+
+        int n = scn.nextInt();
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
         }
+
+        System.out.println("Sum till " + n + " is " + sum + ".");
     }
 }
