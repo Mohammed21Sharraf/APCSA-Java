9a6d6b845edc234d1b343353b737914a079d448a
Mohammed Sharraf <sharraf1212mohammad@gmail.com>
2026-07-14 17:03:37 +0600

    Is a number prime

--- Main.java
+++ Main.java
@@ -1,35 +1,34 @@
 import java.util.Scanner;
-class Main
-{
-    public static void main(String[] args)
-    {
+
+class Main {
+    public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-        int number;
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        char choice;
 
-        do
-        {
-            System.out.print("Enter the number ");
-            number = scn.nextInt();
+        // What to enter:
+        // First enter t = number of test cases
+        // Then enter t numbers (n), one per line
+        System.out.print("Enter number of test cases (t): ");
+        int t = scn.nextInt();
 
-            if(number > max)
-            {
-                max = number;
+        for (int i = 1; i <= t; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int n = scn.nextInt();
+
+            int count = 0;
+            for (int div = 2; div * div <= n; div++) {
+                if (n % div == 0) {
+                    count++;
+                    break;
+                }
             }
 
-            if(number < min)
-            {
-                min = number;
+            if (n <= 1) {
+                System.out.println("not prime");
+            } else if (count == 0) {
+                System.out.println("prime");
+            } else {
+                System.out.println("not prime");
             }
-
-            System.out.print("Do you want to continue y/n? ");
-            choice = scn.next().charAt(0);
-
-        }while(choice=='y' || choice == 'Y');
-
-        System.out.println("Largest number: " + max);
-        System.out.println("Smallest number: " + min);
+        }
     }
 }
